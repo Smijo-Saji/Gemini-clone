69e06d163bfd64e4a83ea0134e57de1014f4087b
Smijo-Saji <Smijo-Saji>
2024-07-29 09:07:19 +0530

    Final

---
+++ src/assets/assets.js
@@ -0,0 +1,33 @@
+import history_icon from "./history_icon_wht.png";
+import menu_icon from "./menu_icon_wht.png";
+import plus_icon from "./plus_icon.png";
+import question_icon from "./question_icon_wht.png";
+import setting_icon from "./setting_icon_wht.png";
+import bulb_icon from "./bulb_icon_wht.png";
+import compass_icon from "./compass_icon_wht.png";
+import gallery_icon from "./gallery_icon_wht.png";
+import mic_icon from "./mic_icon_wht.png";
+import user_icon from "./user_icon1.gif";
+import youtube_icon from "./youtube_icon.png";
+import message_icon from "./message_icon_wht.png";
+import code_icon from "./code_icon_wht.png";
+import send_icon from "./send_icon_wht.png";
+import gemini_icon from "./gemini_icon.png";
+
+export const assets = {
+  history_icon,
+  menu_icon,
+  plus_icon,
+  question_icon,
+  setting_icon,
+  bulb_icon,
+  compass_icon,
+  gallery_icon,
+  mic_icon,
+  user_icon,
+  youtube_icon,
+  message_icon,
+  code_icon,
+  send_icon,
+  gemini_icon,
+};
